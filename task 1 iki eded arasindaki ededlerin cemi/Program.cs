5ce9d8390552f6d008b40ec0bcb270e8039a1eab
Your Name <vahidesh@outlook.com>
2022-05-20 17:02:43 +0400

    Add project files.

---
+++ task 1 iki eded arasindaki ededlerin cemi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace add
+{
+    class sum
+    {
+        static void Main(string[] args)
+        {
+            int from, to;
+
+            Console.Write("ilk ededi daxil edin:  ");
+            from = int.Parse(Console.ReadLine());
+            Console.Write("ikinci ededi daxil edin:  ");
+            to = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = from; i < to; i++)
+            {
+                sum += i;
+
+            }
+            Console.WriteLine("Iki eded arasindaki ededlerin cemi " + sum);
+        }
+
+    }
+}
